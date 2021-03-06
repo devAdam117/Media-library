9aaacc34022e15135bbb6050710ab9fb870335f2
devAdam117 <adam.martinka99@gmail.com>
2021-03-06 19:43:59 +0100

    form+ts not css

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,24 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule} from '@angular/forms'
 
 import { AppComponent } from './app.component';
 import { MediaItemComponent } from './media-item/media-item.component';
 import { MediaItemListComponent } from './media-item-list/media-item-list.component';
 import { TitleComponent } from './title/title.component';
+import { MediaItemFormComponent } from './media-item-form/media-item-form.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     MediaItemComponent,
     MediaItemListComponent,
-    TitleComponent
+    TitleComponent,
+    MediaItemFormComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
